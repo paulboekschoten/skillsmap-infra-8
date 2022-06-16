7d51b7c483c93ec864ae8060de6542c8f5aac461
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-06-16 15:01:55 +0200

    outputs reorganised

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,4 @@
-output "user_private_key_pem" {
-  description = "Private key in PEM format."
-  value       = nonsensitive(tls_private_key.rsa-4096.private_key_pem)
-}
-
-output "public_ip" {
-  description = "The public ip of the web server."
-  value       = aws_instance.paul-web-tf.public_ip
-}
-
-output "certificate_pem" {
-  value = acme_certificate.certificate.certificate_pem
-}
-
-output "issuer_pem" {
-  value = acme_certificate.certificate.issuer_pem
-}
-
-output "cert_private_key_pem" {
-  value = nonsensitive(acme_certificate.certificate.private_key_pem)
+output "dns" {
+  description = "DNS at which the machine is reachable"
+  value = aws_route53_record.www.fqdn
 }
